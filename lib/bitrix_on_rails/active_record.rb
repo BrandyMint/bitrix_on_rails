7a64ddbb693f2834f7e33403172b5ac4f7f6b111
Danil Pismenny <danil@orionet.ru>
2011-08-03 17:55:30 +0400

    PostElement => Post::Element

--- lib/bitrix_on_rails/active_record.rb
+++ lib/bitrix_on_rails/active_record.rb
@@ -7,20 +7,14 @@ module BitrixOnRails
       prop_s_name = "iblock_element_prop_s#{iblock_id}".to_sym
       has_one prop_s_name, :foreign_key => property_name, :class_name => "::IblockElementPropS#{iblock_id}", :autosave => true
 
-      element_class_name = self.name + 'Element'
-      unless Kernel.const_defined?(element_class_name)
-        element_class = Class.new(IblockElement) do
-          set_iblock_id iblock_id
-        end
-
-        if block_given?
-          element_class.instance_eval(&blk)
-        end
-
-        Kernel.const_set(element_class_name, element_class)
+      element_class = Class.new IblockElement do
+        set_iblock_id 3
       end
+      element_class.class_eval(&blk) if block_given?
 
-      has_one :iblock_element, :through => prop_s_name, :class_name => "::#{element_class_name}", :autosave => true
+      self.const_set('Element', element_class)
+
+      has_one :iblock_element, :through => prop_s_name, :class_name => 'Element', :autosave => true
     end
   end
 end
